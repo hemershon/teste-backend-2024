f74b52b602eb225d0c58821f121ee6cb02f1a858
Hemershon Silva <hemershon@gmail.com>
2024-05-02 20:02:38 -0300

    feat create test for model and controller

--- ms-rails/test/controllers/api/v1/products_controller_test.rb
+++ ms-rails/test/controllers/api/v1/products_controller_test.rb
@@ -7,14 +7,17 @@ class Api::V1::ProductsControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
+    assert_not_nil @product
     get :show, params: { id: @product.id }
     assert_response :success
   end
 
   test "should update product" do
-    put :update, params: { id: @product.id, product: { name: "New Name", brand: "New Brand" } }
+    assert_not_nil @product
+    put :update, params: { id: @product.id, product: { name: "Laptop", brand: "keyd" } }
     @product.reload
-    assert_equal "New Name", @product.name
+    assert_equal "Laptop", @product.name
     assert_response :success
   end
+
 end
